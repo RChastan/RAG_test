14aa6c65e7a00720931220f2444d6c05d9717b24
MickaRiv <60695230+MickaRiv@users.noreply.github.com>
2023-11-18 17:38:27 +0100

    ajout de orca et open llama

--- llama2_test_GGML.py
+++ llama2_test_GGML.py
@@ -8,7 +8,8 @@ tokenizer = AutoTokenizer.from_pretrained(model)
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
-    torch_dtype=torch.float16
+    torch_dtype=torch.float16,
+    device_map="auto"
 )
 
 sequences = pipeline(
